051628e96d91df3afb57baf3f18ff6602daf5fcf
Mark Booth <mark@markandpol.com>
2020-04-20 14:29:37 +0100

    Initial Commit

---
+++ Twiddle WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Twiddle WatchKit Extension
+//
+//  Created by Mark Booth on 20/04/2020.
+//  Copyright © 2020 Mark Booth. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Twiddle WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  Twiddle WatchKit Extension
+//
+//  Created by Mark Booth on 20/04/2020.
+//  Copyright © 2020 Mark Booth. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
